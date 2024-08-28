7ff281d3ac29b13d4c7cb2181931318c052f1527
ysinha <ysinha@pingidentity.com>
2024-08-28 16:03:35 +0530

    Testing thanos config

--- tests/monitoring/test_thanos_configuration.py
+++ tests/monitoring/test_thanos_configuration.py
@@ -2,15 +2,13 @@ import unittest
 import boto3
 import os
 import logging
-from kubernetes import client, config
-from botocore.exceptions import ClientError
+from k8s_utils import K8sUtils
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 class TestThanos(unittest.TestCase):
     def setUp(self):
-        self.iam_client = boto3.client('iam')
-        self.ssm_client = boto3.client('ssm')
+        self.k8s_utils = K8sUtils()
         self.s3_client = boto3.client('s3')
         
         self.cluster_name = os.getenv("CLUSTER_NAME")
@@ -19,47 +17,26 @@ class TestThanos(unittest.TestCase):
             self.fail("CLUSTER_NAME environment variable is required but not set.")
 
         self.bucket_name = f"{self.cluster_name}-thanos-bucket"
-        self.environments = os.getenv("SUPPORTED_ENV", "dev,test,stage,prod").split(',')
 
-        config.load_kube_config()
-        self.k8s_client = client.CoreV1Api()
-
-    def test_irsa_role_exists(self):
-        role_name = f"{self.cluster_name}-irsa-thanos"
+    def test_check_logs_in_s3_bucket(self):
         try:
-            self.iam_client.get_role(RoleName=role_name)
-        except self.iam_client.exceptions.NoSuchEntityException:
-            self.fail(f"IRSA role {role_name} not found")
-
-    def test_iam_policy_attachment(self):
-        policy_name = f"{self.cluster_name}-thanos-irsa-policy"
-        role_name = f"{self.cluster_name}-irsa-thanos"
-        attached_policies = self.iam_client.list_attached_role_policies(RoleName=role_name)['AttachedPolicies']
-        self.assertIn(policy_name, [policy['PolicyName'] for policy in attached_policies], f"Policy {policy_name} not attached to role {role_name}")
-
-    def test_ssm_parameters(self):
-        for env in self.environments:
-            param_name = f"/{self.cluster_name}/pcpt/config/k8s-config/accounts/{env}/irsa-role/thanos/arn"
-            try:
-                self.ssm_client.get_parameter(Name=param_name)
-            except self.ssm_client.exceptions.ParameterNotFound:
-                self.fail(f"SSM parameter {param_name} not found")
-
-    def test_s3_bucket_exists(self):
-        try:
-            self.s3_client.head_bucket(Bucket=self.bucket_name)
+            logs_objects = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
+            self.assertIsNotNone(logs_objects.get('Contents'), f"No logs found in S3 bucket {self.bucket_name}")
         except self.s3_client.exceptions.NoSuchBucket:
             self.fail(f"S3 bucket {self.bucket_name} not found")
 
-    def test_thanos_pods_running(self):
+    def test_thanos_pods_running_and_ready(self):
         labels = ["storegateway", "compactor", "receive"]
         for label in labels:
             logging.info(f"Checking pods with label: {label}")
-            pods = self.k8s_client.list_namespaced_pod(namespace="prometheus", label_selector=f"app.kubernetes.io/component={label}").items
-            self.assertGreater(len(pods), 0, f"No pods found for {label}")
+            
+            self.assertTrue(self.k8s_utils.wait_for_pod_running(label, "prometheus"), f"Pods with label {label} are not running")
+            self.assertTrue(self.k8s_utils.wait_for_pod_ready(label, "prometheus"), f"Pods with label {label} are not ready")
+            
+            pods = self.k8s_utils.get_pods(label, "prometheus")
             for pod in pods:
-                self.assertEqual(pod.status.phase, "Running", f"Pod {pod.metadata.name} not running")
-                self.assertEqual(pod.status.container_statuses[0].restart_count, 0, f"Pod {pod.metadata.name} has restarts")
+                restart_count = pod.status.container_statuses[0].restart_count
+                self.assertEqual(restart_count, 0, f"Pod {pod.metadata.name} with label {label} has restarted (restart count: {restart_count})")
 
 if __name__ == "__main__":
     unittest.main()
