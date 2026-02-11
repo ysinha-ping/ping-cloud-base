4290f5c5c1e20a0511e26c28b21364fda3597909
ysinha <ysinha@pingidentity.com>
2026-02-11 18:55:44 +0530

    add overlay patch values

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -476,7 +476,8 @@ ${DASH_REPO_BRANCH}
 ${APP_RESYNC_SECONDS}
 ${CERT_RENEW_BEFORE}
 ${TELEPORT_RESOURCE_ID}
-${STAGE}'
+${STAGE}
+${SIZE}'
 
 # Variables to replace within the generated cluster state code
 REPO_VARS="${REPO_VARS:-${DEFAULT_VARS}}"
